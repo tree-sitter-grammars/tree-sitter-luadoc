ae28906932d20af4c921140713985ee9a409780c
Amaan Qureshi <amaanq12@gmail.com>
2023-03-19 08:07:42 -0400

    feat: update queries accordingly

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -8,20 +8,31 @@
   "@field"
   "@generic"
   "@vararg"
+  "@diagnostic"
+  "@deprecated"
+  "@cast"
+  "@overload"
+  "@enum"
   "@language"
   "@see"
   "extends"
 ] @keyword
 
+[
+  "@async"
+] @keyword.coroutine
+
 (language_injection "@language" (identifier) @keyword)
 
-[
-  "fun"
-] @keyword.function
+(function_type ["fun" "function"] @keyword.function)
 
 [
   "@return"
 ] @keyword.return
+
+; Debugs
+
+(diagnostic_identifier) @debug
 
 ; Qualifiers
 
@@ -29,23 +40,53 @@
   "public"
   "protected"
   "private"
+  "@public"
+  "@protected"
+  "@private"
 ] @type.qualifier
+
 
 ; Variables
 
 (identifier) @variable
 
+[
+  "..."
+  "self"
+] @variable.builtin
+
+; Macros
+
+(alias_annotation (identifier) @function.macro)
+
 ; Parameters
+
+(param_annotation (identifier) @parameter)
 
 (parameter (identifier) @parameter)
 
+; Fields
+
+(field_annotation (identifier) @field)
+
+(table_literal_type field: (identifier) @field)
+
+(member_type ["#" "."] . (identifier) @field)
+
 ; Types
 
-"table" @type.builtin
+(table_type "table" @type.builtin)
 
 (builtin_type) @type.builtin
 
-(type (identifier) @type)
+(class_annotation (identifier) @type)
+
+(enum_annotation (identifier) @type)
+
+((array_type ["[" "]"] @type)
+  (#set! "priority" 105))
+
+(type) @type
 
 ; Operators
 
@@ -57,9 +98,13 @@
 
 (literal_type) @string
 
+(number) @number
+
 ; Punctuation
 
 [ "[" "]" ] @punctuation.bracket
+
+[ "{" "}" ] @punctuation.bracket
 
 [ "(" ")" ] @punctuation.bracket
 
@@ -67,21 +112,25 @@
 
 [
   ","
+  "."
   "#"
   ":"
 ] @punctuation.delimiter
+
+[
+  "@"
+  "?"
+] @punctuation.special
 
 ; Comments
 
 (comment) @comment @spell
 
 (at_comment 
-  "@" @punctuation.special 
   (identifier) @type 
-  (_) @comment)
+  (_) @comment @spell)
 
 (class_at_comment 
-  "@" @punctuation.special 
   (identifier) @type 
   ("extends"? (identifier)? @type)
-  (_) @comment)
+  (_) @comment @spell)
